8ab025b5a9cd658145ec505678b3682bc7486652
michalsn <michal@sniatala.pl>
2023-04-26 19:16:14 +0200

    escape book on display

--- src/Views/books/table_row.php
+++ src/Views/books/table_row.php
@@ -1,7 +1,7 @@
 <tr>
     <td><?= $book->id; ?></td>
-    <td><?= $book->title; ?></td>
-    <td><?= $book->author; ?></td>
+    <td><?= esc($book->title); ?></td>
+    <td><?= esc($book->author); ?></td>
     <td class="text-end">
         <a hx-get="<?= site_url('books/edit/' . $book->id); ?>" hx-target="closest tr" hx-swap="outerHTML" class="btn btn-sm btn-outline-primary">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4"></path><line x1="13.5" y1="6.5" x2="17.5" y2="10.5"></line></svg>
